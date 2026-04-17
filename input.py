e5c7ebd874932e45f053a1058a60ead813da560d
Almas Sayem <mdassayem0@gmail.com>
2026-04-17 13:11:24 +0600

    change

---
+++ input.py
@@ -0,0 +1,6 @@
+name = input("Enter name:")
+age = int(input("Enter age:"))
+marks = float(input("Enter marks:"))
+print("Welcome",name)
+print("Age =",age)
+print("Marks=",marks)
---
+++ pracrice_problem1.py
@@ -0,0 +1,18 @@
+"""
+Write a program to print 2 number 
+& print their add,sub,mult,div,reminder
+"""
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+print("Addition=",a+b)
+print("Substraction=",a-b)
+print("Multiplication=",a*b)
+print("Divition=",a/b)
+print("Reminder=",a%b)
+"""
+Write a program to input side of a square
+& print it's area
+"""
+side=float(input("Enter square side:"))
+print("Area=",side*side)
+print("Area=",side**2)
