22b6b128d6146afe83ba544fdccc80144a4c91dc
Almas Sayem <mdassayem0@gmail.com>
2026-04-17 19:31:54 +0600

    Update string.py

--- string.py
+++ string.py
@@ -1,1 +1,26 @@
-print("Hello")
+str1="this is a String.\nWe are creating it in python"
+print(str1)
+#concretenation
+str1="Almas"
+len1=len(str1)
+print(len1)
+str2="Sayem"
+len2=len(str2)
+print(len2)
+Final_str=str1+" "+str2
+print(Final_str)
+print(len(Final_str))
+#indexing
+str="Almas Sayem"
+print(str[0])
+print(str[1])
+print(str[2])
+
+# str[starting_index:ending_index]
+
+str="Almas Sayem"
+print(str[1:4])
+print(str[0:len(str)])
+print(str[:5])
+print(str[6:])
+print(str[-3:-1])
