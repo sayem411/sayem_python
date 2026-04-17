24a5b49560c17506db8a6a0b10e9aa9f5b315add
Almas Sayem <mdassayem0@gmail.com>
2026-04-17 12:31:49 +0600

    Update operator.py

--- operator.py
+++ operator.py
@@ -1,1 +1,26 @@
-print("Hi")
+a=5
+b=2
+#arithmatic operator
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+#relational operator
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a<b)
+print(a>b)
+#assignment operatior
+num=10;
+num=num+10
+print("A is:",num)
+#logical ooperator
+val1=True
+val2=False
+print("AND operator:",val1 and val2)
+print("OR operator:",val1 or val2)
+print("OR operator:",(a==b)or(a>b))
