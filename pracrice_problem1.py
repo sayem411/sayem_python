0f77f3b07f710c41e8e8e10deb961f32f6d2de7c
Almas Sayem <mdassayem0@gmail.com>
2026-04-17 14:42:17 +0600

    Update pracrice_problem1.py

--- pracrice_problem1.py
+++ pracrice_problem1.py
@@ -16,3 +16,18 @@ Write a program to input side of a square
 side=float(input("Enter square side:"))
 print("Area=",side*side)
 print("Area=",side**2)
+"""
+Write a program to input 2 floting point number 
+and point their average
+"""
+a=float(input("Enter a:"))
+b=float(input("Enter b:"))
+print("Average=",(a+b)/2)
+"""
+Write a program to input 2 number a and b
+print True if a is greater than or equal to b .
+If not print False 
+"""
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+print(a>=b)
