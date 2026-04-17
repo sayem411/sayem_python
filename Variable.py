e2275857400c9d51e3de575c38de75978c25a6e6
Almas Sayem <mdassayem0@gmail.com>
2026-04-17 11:44:59 +0600

    Update Variable.py

--- Variable.py
+++ Variable.py
@@ -2,7 +2,15 @@ name="sayem"#'sayem' or '''sayem'''
 age=21
 age2=age
 price=25.9
+old=False
+a=None
 print("My name is:",name)
 print("My age is:",age2)
 print("price is:",price)
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
 
