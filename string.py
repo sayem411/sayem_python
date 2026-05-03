5577150a635d7826c4df72533d04bde3d056a498
Almas Sayem <mdassayem0@gmail.com>
2026-05-04 01:41:56 +0600

    updated

--- string.py
+++ string.py
@@ -5,6 +5,9 @@ str1="Almas"
 len1=len(str1)
 print(len1)
 str2="Sayem"
+str3="AS"
+stgg="{} {},{}!".format(str1,str2,str3)
+print(stgg)
 len2=len(str2)
 print(len2)
 Final_str=str1+" "+str2
@@ -25,3 +28,33 @@ print(str[0:len(str)])
 print(str[:5])
 print(str[6:])
 print(str[-3:-1]) 
+
+
+str="I am a coder"
+print(str.endswith("oder"))#True
+print(str.endswith("Hi"))#False
+#Capitalize 1st character
+str1="hi Sayem"
+str1=str1.capitalize()
+print(str1)
+#Replace
+print(str.replace("coder","Doctor"))
+#find
+print(str.find("coder"))
+#count
+print(str.count("a"))
+#index
+print(str.index('c'))
+#splits
+print(str.split(" "))
+#rpartition
+print(str.rpartition(" a "))
+
+stg="Simplilearn"
+print(stg.upper())
+print(stg.lower())
+for i in stg:
+    print(i,end="")
+
+
+
--- tuples.py
+++ tuples.py
@@ -5,15 +5,56 @@ print(type(emp))
 print(emp)
 
 city=("Dhaka","Rajshahi","Chittagong")
+num=1,2,3,4
 print(city)
 print(type(city))
 print(city[1])
 print(city[-1])
+print(city+num)
 
 list=[1,2,3]
 tuple=(1,2,3)
 list.append(4)
 print(list) #'tuple' object has no attribute 'append'
+
+#Nesting
+nest=(city,num)
+print(nest)
+
+#Repetation
+rep=("Python",)
+print(rep*10)
+
+a,*b,c,d=num
+print(a,b,c,d)
+
+#Built in function
+num2=(3,4,2,2,2,2,6,5,8)
+print(num2)
+print(num2.count(2))
+print(sum(num2))
+print(len(num2))
+print(max(num2))
+print(min(num2))
+
+#converting tuple to list
+lst=[(1,2,3),(4,5,6)]
+print(lst)
+lst.append(("Tuple","Inside","List"))
+print(lst)
+lst.remove((1,2,3))
+print(lst)
+
+#converting list to tuples
+tpl=(['a','b','c'],['e','f','g'])
+print(tpl)
+
+tpl[0].append('d')
+print(tpl)
+tpl[0].remove('d')
+print(tpl)
+
+
 
 
 
