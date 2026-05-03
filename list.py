54d5d79d2f71e409e7f4203badd90480d937907b
Almas Sayem <mdassayem0@gmail.com>
2026-05-04 00:05:53 +0600

    updated

--- list.py
+++ list.py
@@ -19,5 +19,39 @@ print(var)
 one, *other=num
 print(one)
 print(other)
-#methods in lists
-#26:40 minute simplilearn
+#method in list
+num.append(4)
+print(num)
+
+num.extend(stg)
+print(num)
+
+num.insert(3,"simplilearn")
+print(num)
+
+num.remove("simplilearn")
+print(num)
+
+var1=['a','s','d','f','g']
+var1.sort()
+print(var1)
+
+#Build-in-function with list
+x=[9,17,14,4,90,55]
+print(len(x))
+print(min(x))
+print(max(x))
+print(sum(x))
+print(sum(x)/len(x))
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tuples.py
@@ -0,0 +1,19 @@
+#Tuples is collection of immutable heterogeneous python objects
+#creating tuples
+emp=()
+print(type(emp))
+print(emp)
+
+city=("Dhaka","Rajshahi","Chittagong")
+print(city)
+print(type(city))
+
+list=[1,2,3]
+tuple=(1,2,3)
+list.append(4)
+tuple.append(4)
+print(list)
+print(tuple)#'tuple' object has no attribute 'append'
+
+
+
