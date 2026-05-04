7c0b6dea367befc27a23a292c2392fc29d4aeae9
Almas Sayem <mdassayem0@gmail.com>
2026-05-04 19:56:10 +0600

    updated

--- Conditional_Statement.py
+++ Conditional_Statement.py
@@ -1,3 +1,12 @@
+i=24
+if(i<25):
+    if(i%2==0):
+        print("i is Even less than 25 ")
+    else:
+        print("i is odd less than 25")
+else:
+    print("i is greater than 25")
+
 age=21
 if(age==18):
     print(" 1st step to apply for license")
