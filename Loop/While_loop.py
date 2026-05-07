dd747d43e4b05988d8c10de05769043a62b7e740
Almas Sayem <mdassayem0@gmail.com>
2026-05-07 06:13:34 +0600

    Update While_loop.py

--- Loop/While_loop.py
+++ Loop/While_loop.py
@@ -3,8 +3,16 @@ while(val%7!=0):
     val=int(input("Enter a multiple of 7: "))
 else:
     print("%d is a multiple of 7"%val)
+#print 10 times
+i=1
+while i<=10:
+    print("Simplilearn")
+    i+=1
 
-
+i=10
+while i>=1:
+    print("Simplilearn")
+    i-=1
 
 
 
