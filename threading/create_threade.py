59f8bd36f08340fce2a43a4f3f3f602d8329510e
Almas Sayem <mdassayem0@gmail.com>
2026-05-08 22:34:44 +0600

    updated

--- threading/create_threade.py
+++ threading/create_threade.py
@@ -7,4 +7,23 @@ print("This is a parent thread")
 
 #Threading package
 from threading import *
+class mythread(Thread):
+    def run(self):
+        for i in range(5):
+            print("\nThis is a child class")
+t=mythread()
+t.start()
+for i in range(5):
+    print("\nThis is the main thread")
 
+#obj
+from threading import *
+class demo:
+    def show(self):
+        for i in range(5):
+            print("\nThis is a child class")
+obj=demo()
+t=Thread(target=obj.show())
+t.start()
+for i in range(5):
+    print("\nThis is the parent thread")
