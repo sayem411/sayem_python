52a8ebc34237a9d536a5685b46ab0cf9d881f09f
Almas Sayem <mdassayem0@gmail.com>
2026-05-09 22:43:58 +0600

    updated

---
+++ PythonLibraries_Numpy/My_numpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+a=np.array([1,2,3])
+print(type(a))
+print(a.shape)
+
+b=np.arange(12).reshape(3,4)
+print(b)
--- PythonLibraries_Numpy/numpyy.py
+++
@@ -1,3 +0,0 @@
-import PythonLibraries_Numpy.numpyy as np
-a=np.array([1,2,3])
-print(type(a))
