6fd0f8434f62989cfb577d583788155ba09e5bb3
Almas Sayem <mdassayem0@gmail.com>
2026-05-10 14:46:02 +0600

    Update My_numpy.py

--- PythonLibraries_Numpy/My_numpy.py
+++ PythonLibraries_Numpy/My_numpy.py
@@ -34,6 +34,30 @@ print("Python list tool:",(time.time() -start)*1000)
 #---------------------------------
 a=np.array([[1,2],[3,4],[5,6]])
 print(a)
+print(a.ndim)
+print(a.itemsize)
+print(a.shape)
+a=np.array([[1,2],[3,4],[5,6]],dtype=np.float64)
+print(a)
+print(a.itemsize)
+print(a.shape)
+a=np.array([[1,2],[3,4],[5,6]],dtype=complex)
+print(a)
+print(np.zeros((3,4)))
+print(np.ones((3,4)))
+l=range(5)
+print(l)
+l=np.arange(5)
+print(l)
+print('concatenation example:')
+print(np.char.add(['hello','hi'],['world','xyz']))
+print(np.char.multiply('hello\t',3))
+print(np.char.center('hello',20,fillchar='-'))
+
+
+
+
+
 
 
 
