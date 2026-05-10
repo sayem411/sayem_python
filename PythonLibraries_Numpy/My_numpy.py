7b1cd0fb7648fd6184eb1ffb76862b12c8681634
Almas Sayem <mdassayem0@gmail.com>
2026-05-10 11:03:08 +0600

    updated

--- PythonLibraries_Numpy/My_numpy.py
+++ PythonLibraries_Numpy/My_numpy.py
@@ -1,5 +1,6 @@
 import numpy as np
 a=np.array([1,2,3])
+print(a)
 print(type(a))
 print(a.shape)
 
@@ -7,3 +8,30 @@ b=np.arange(12).reshape(3,4)
 print(b)
 b.reshape(3,4)
 print(b)
+#numpy vs python list
+import time
+import sys
+c=range(1000)
+print(sys.getsizeof(5)*len(c))
+
+d=np.arange(1000)
+print(d.size*d.itemsize)
+
+size=1000000
+L1=range(size)
+L2=range(size)
+A1=np.arange(size)
+A2=np.arange(size)
+start=time.time()
+result=[(x+y) for x,y in zip(L1,L2)]
+result=[(x+y) for x,y in zip(L1,L2)]
+print(result)
+print("Python list tool:",(time.time() -start)*1000)
+
+start = time.time()
+result=A1 +A2
+print(result)
+
+
+
+
--- PythonLibraries_Numpy/lib_matplotlib.py
+++ PythonLibraries_Numpy/lib_matplotlib.py
@@ -1,6 +1,6 @@
+#Area
 import numpy as np
 import matplotlib.pyplot as plt
-
 np.random.seed(10)
 N=30
 x=np.random.rand(N)
@@ -8,4 +8,20 @@ y=np.random.rand(N)
 colors=np.random.rand(N)
 area=(30*np.random.rand(N))**2
 plt.scatter(x,y,s=area,c=colors,alpha=0.4)
+plt.show()
+#Graph
+from matplotlib import style
+style.use('ggplot')
+x=[2,4,6]
+y=[12,14,16]
+
+x2=[3,5,7]
+y2=[7,14,15]
+
+plt.bar(x,y,color='r',align='center')
+plt.bar(x2,y2,color='g',align='center')
+
+plt.title('Info')
+plt.ylabel('Y axis')
+plt.xlabel('X axis')
 plt.show()
