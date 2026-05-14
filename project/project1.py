406ea8ddd75bb76128c7402db3ee3c30541b485a
Almas Sayem <mdassayem0@gmail.com>
2026-05-14 23:52:30 +0600

    Update project1.py

--- project/project1.py
+++ project/project1.py
@@ -1,10 +1,33 @@
-print(" student profile")
-name=input("Enter name:")
-number=int(input("Enter number:"))
-ugmail=input("Enter University gmail:")
-pgmail=input("Enter personal gmail:")
-gender=input("Enter gender:")
+#Strong password
+password = input("Enter password: ")
 
-print("name:",name)
-print("Mobile Number:",number)
+has_upper=False
+has_lower=False
+has_digit=False
+has_special=False
 
+for i in password:
+    if i.isupper():
+        has_upper=True
+    elif i.islower():
+        has_lower=True
+    elif i.isdigit():
+        has_digit=True
+    else:
+        has_special=True     
+
+if has_upper and has_lower and has_digit and has_special and len(password) >= 8:
+    print("Strong password ")
+else:
+    print("Weak password ")
+    
+    if not has_upper:
+        print("— Add uppercase letter (A-Z)")
+    if not has_lower:
+        print("— Add lowercase letter (a-z)")
+    if not has_digit:
+        print("— Add a number (0-9)")
+    if not has_special:
+        print("— Add special character (!@#$...)")
+    if len(password) < 8:
+        print("— Minimum 8 characters required")
